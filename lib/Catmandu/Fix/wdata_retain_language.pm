20634506d12f334a634eeae635932cc49a19a2c7
Jakob Voß <jakob.voss@gbv.de>
2013-12-09 09:27:11 +0000

    Import of VOJ/Catmandu-Wikidata-0.02 from CPAN.
    gitpan-cpan-distribution: Catmandu-Wikidata
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         VOJ/Catmandu-Wikidata-0.02.tar.gz
    gitpan-cpan-author:       VOJ
    gitpan-cpan-maturity:     released

--- lib/Catmandu/Fix/wdata_retain_language.pm
+++ lib/Catmandu/Fix/wdata_retain_language.pm
@@ -1,10 +1,12 @@
-package Catmandu::Fix::wdata_limit_language;
+package Catmandu::Fix::wdata_retain_language;
 #ABSTRACT: Limit string values to a selected language
-our $VERSION = '0.01'; #VERSION
+our $VERSION = '0.02'; #VERSION
 use Catmandu::Sane;
 use Moo;
 
 has language => (is => 'ro', required => 1);
+
+has force => (is => 'ro');
 
 around BUILDARGS => sub {
     my ($orig, $class, $language) = @_;
@@ -25,6 +27,7 @@ sub fix {
             map { $_->{value} } @{$data->{aliases}->{$language}} 
         } ]; # TODO: how to express this as normal fix with move_field?
 
+    # TODO: only delete of string of requested language was found (or force)
     delete $data->{$_} for qw(aliases labels descriptions);
 
     $data;
@@ -40,24 +43,29 @@ __END__
 
 =head1 NAME
 
-Catmandu::Fix::wdata_limit_language - Limit string values to a selected language
+Catmandu::Fix::wdata_retain_language - Limit string values to a selected language
 
 =head1 VERSION
 
-version 0.01
+version 0.02
 
 =head1 DESCRIPTION
 
-This L<Catmandu::Fix> modifies a Wikidata entity record by limiting the values of
-C<aliases>, C<labels>, and C<descriptions> to a selected language. The fix
+This L<Catmandu::Fix> modifies a Wikidata entity record, as imported by
+L<Catmandu::Importer::Wikidata>, by deleting all language tagged strings (in
+C<aliases>, C<labels>, and C<descriptions>) expect a selected language.  The
+fix
 
-    wdata_limit_language('fr');
+    wdata_retain_language('fr');
 
 is roughly equivalent to
 
     move_field('labels.fr.value','label');
     move_field('descriptions.fr.value','description');
-    move_field('aliases.fr.*.value','alias.$append');     # FIXME
+    move_field('aliases.fr.*.value','alias.$append');
+
+Modification of additional fields may be added in a future release of this
+module.
 
 =head1 AUTHOR
 
--- lib/Catmandu/Wikidata.pm
+++ lib/Catmandu/Wikidata.pm
@@ -2,7 +2,7 @@ use strict;
 use warnings;
 package Catmandu::Wikidata;
 #ABSTRACT: Import from Wikidata for processing with Catmandu
-our $VERSION = '0.01'; #VERSION
+our $VERSION = '0.02'; #VERSION
 
 
 1;
@@ -19,7 +19,7 @@ Catmandu::Wikidata - Import from Wikidata for processing with Catmandu
 
 =head1 VERSION
 
-version 0.01
+version 0.02
 
 =head1 SYNOPSIS
 
@@ -28,7 +28,7 @@ version 0.01
     catmandu convert Wkidata --title dewiki:Metadaten to JSON --pretty
 
     catmandu convert Wikidata --title "Emma Goldman" \
-        --fix "wdata_limit_language('en')" to JSON --pretty
+        --fix "wdata_retain_language('en')" to JSON --pretty
 
 =head1 DESCRIPTION
 
@@ -43,16 +43,23 @@ within the L<Catmandu> framework.
 
 Import entities from L<http://www.wikidata.org/>.
 
-=item L<Catmandu::Fix::wdata_limit_language>
+=item L<Catmandu::Fix::wdata_retain_language>
 
-Provides the fix C<wdata_limit_language($language)> to limit the values of
+Provides the fix C<wdata_retain_language($language)> to limit the values of
 field C<aliases>, C<labels>, and C<descriptions> to a selected language. 
+
+=item L<Catmandu::Fix::wdata_simplify_claims>
+
+Simplfies the C<claims> field of a Wikidata entity record.
 
 =back
 
 =head1 SEE ALSO
 
-* <http://librecat.org/>
+Background information on Catmandu can be found at <http://librecat.org/>.
+
+Background information on Wikidata can be found at
+<http://www.wikidata.org/wiki/Wikidata:Introduction>.
 
 =head1 AUTHOR
 
